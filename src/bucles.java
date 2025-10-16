56be03221e61cd5b5f6254b6e176246550afa780
Iker Santos Insua <ikersantosinsua@gmail.com>
2025-10-16 13:10:27 +0200

    Ejercicio 3 acabado

--- src/bucles.java
+++ src/bucles.java
@@ -23,7 +23,7 @@ public class bucles {
         // refresco y tu cambio: X €". Si no, muestra "Saldo insuficiente 😞".
         // Permite repetir la compra con un bucle while hasta que el usuario introduzca saldo suficiente.
 
-        System.out.println("Cuál es tu saldo?: ");
+        System.out.println("\nCuál es tu saldo?: ");
         int saldo = scanner.nextInt();
         System.out.println("Cuánto te valió el refresco?: ");
         int refresco = scanner.nextInt();
@@ -31,10 +31,46 @@ public class bucles {
         int cambio = saldo - refresco;
 
         while (saldo > refresco) {
-            System.out.println("Aquí tienes tu refresco y tu cambio: " + cambio + "€");
-            System.out.println("Cuánto te valió el siguiente refresco?: ");
-            int nuevoRefresco = scanner.nextInt();
+            System.out.println("Aquí tienes tu refresco y tu cambio: "  + cambio + "€");
+            saldo -= refresco;
+            cambio++;
         }
-            System.out.println("Saldo insuficiente \uD83D\uDE1E");
+
+        // EJERCICIO 3 "Batalla Pokémon Simplificada"
+        // Pide al usuario los puntos de vida (HP) de su Pokémon y los del rival.
+        // Mientras ambos tengan vida (while), resta daño en cada turno y muestra el resultado. no tienes que elegir tipo de ataque; simplemente, resta un daño concreto que has guardado en una variable previamente.
+        // Usa condicionales para determinar quién gana: "Tu Pokémon ha ganado!" o "Has perdido...".
+
+        System.out.println("\nCuántos puntos de vida tiene tu Charizard?: ");
+        int vida = scanner.nextInt();
+        System.out.println("Y los de tu rival Venusaur?: ");
+        int vidaRival = scanner.nextInt();
+
+        int danho = 20;
+        int turno = 1;
+        boolean turnoPokemon = true;
+
+        while (vida > 0 && vidaRival > 0) {
+            System.out.println("\nTurno " + turno++);
+            System.out.println("\nContendientes: ");
+            System.out.println("Charizard" + "\nVida: " + vida);
+            System.out.println("Venusaur" + "\nVida: " + vidaRival);
+            if (turnoPokemon) {
+                System.out.println("\nCharizard pierde " + danho + " puntos de vida");
+                vida -= danho;
+                System.out.println("Vida restante de Charizard: " + vida);
+
+            } else {
+                System.out.println("\nVenusaur pierde " + danho + " puntos de vida");
+                vidaRival -= danho;
+                System.out.println("Vida restante de Venusaur: " + vidaRival);
+            }
+            turnoPokemon = !turnoPokemon;
+        }
+        if (vida <= 0) {
+            System.out.println("\nTu pokémon Charizard ha perdido!!");
+        } else if (vidaRival <= 0) {
+            System.out.println("\nTu pokémon Charizard ha ganado!!");
+        }
     }
 }

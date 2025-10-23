094f452953a747975bf68d8fa90a37f3be20cea6
Iker Santos Insua <ikersantosinsua@gmail.com>
2025-10-23 11:59:33 +0200

    Ejercicio 2 funciones acabado

--- src/funciones.java
+++ src/funciones.java
@@ -6,6 +6,11 @@ public class funciones {
         System.out.println(paraImprimir);
     }
 
+    public static int sumar(int a, int b) {
+        int suma = a + b;
+        return suma;
+    }
+
     public static void main(String[] args) {
         // EJERCICIO 1 "Saludo personalizado"
         // Crea una función estática llamada saludar() que reciba un nombre como parámetro (String)
@@ -17,5 +22,20 @@ public class funciones {
         Scanner scanner = new Scanner(System.in);
         String nombrePorTeclado = scanner.next();
         saludar(nombrePorTeclado);
+
+        // EJERCICIO 2 "Suma de dos números"
+        // Crea una función estática llamada sumar() que reciba dos números enteros
+        // y devuelva su suma. En el main, pide los dos números, llama a la función
+        // y muestra el resultado con un mensaje como: "La suma de A + B es = X".
+
+        System.out.println("\nDame un número entero: ");
+        Scanner escanerNumeroEntero = new Scanner(System.in);
+        System.out.println("Dame otro número entero: ");
+        Scanner escanerOtroNumeroEntero = new Scanner(System.in);
+
+        int suma = (escanerNumeroEntero.nextInt() +  escanerOtroNumeroEntero.nextInt());
+        sumar(suma, suma);
+
+        System.out.println("La suma de A + B es: " + suma);
     }
 }

17a171211a98fec158cdd4aaacdf1fe0ec477d97
Iker Santos Insua <ikersantosinsua@gmail.com>
2025-10-30 13:48:47 +0100

    Ejercicio 2 básico Arrays terminado

--- src/Arrays.java
+++ src/Arrays.java
@@ -18,7 +18,7 @@ public class Arrays {
         objetosMinecraft[4] = "antorchas";
 
         for (int posicionActual = 0;
-             posicionActual<objetosMinecraft.length;
+             posicionActual < objetosMinecraft.length;
              posicionActual++) {
             String paraImprimir = "Tienes en tu inventario: " + objetosMinecraft[posicionActual];
 
@@ -26,5 +26,43 @@ public class Arrays {
         }
         int cantidadObjetos = objetosMinecraft.length;
         System.out.println("\nEl jugador tiene " + cantidadObjetos + " objetos");
+
+
+        // EJERCICIO 2 - Puntuaciones de juego
+        // Crea un array con las puntuaciones de 6 partidas (valores enteros).
+        // Recorre el array para calcular:
+        // Puntuación total.
+        // Media de puntos por partida.
+        // Mayor puntuación conseguida.
+        // Muestra todo con mensajes tipo:
+        // "Tu récord fue de 2380 puntos en una partida."
+
+        int[] puntuaciones = new int[6];
+
+        puntuaciones[0] = 97;
+        puntuaciones[1] = 63;
+        puntuaciones[2] = 65;
+        puntuaciones[3] = 14;
+        puntuaciones[4] = 91;
+        puntuaciones[5] = 25;
+
+        int puntuacionTotal = 0;
+        int mayorPuntuacion = puntuaciones[0];
+
+        for ( int posicionActualJuego = 0;
+              posicionActualJuego<puntuaciones.length;
+              posicionActualJuego++) {
+            if (puntuaciones[posicionActualJuego] > mayorPuntuacion) {
+                mayorPuntuacion = puntuaciones[posicionActualJuego];
+            }
+            puntuacionTotal += puntuaciones[posicionActualJuego];
+        }
+        int media = puntuacionTotal / puntuaciones.length;
+        String paraImprimirPuntuaciones = "\nTu récord fue de " + puntuacionTotal +
+                " puntos en una partida" + "\nTu puntuación media fue de " + media +
+                " puntos en una partida" + "\nTu mayor puntuación conseguida en una partida fue de " +
+                mayorPuntuacion + " puntos";
+
+        System.out.println(paraImprimirPuntuaciones);
     }
 }

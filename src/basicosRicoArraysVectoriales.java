9a9629e1641d209a57aacb86d5db8b8211aaab81
Iker Santos Insua <ikersantosinsua@gmail.com>
2025-12-04 12:56:51 +0100

    Comparativa Ejercicio 5 acabado

--- src/basicosRicoArraysVectoriales.java
+++ src/basicosRicoArraysVectoriales.java
@@ -61,7 +61,8 @@ public class basicosRicoArraysVectoriales {
         System.out.println("\n--Puntuaciones de la partida--");
         System.out.println("Puntuación más alta (MVP): " + puntuacionMax);
         System.out.println("Puntuación más baja: " + puntuacionMin);
-*/
+
+
         // Ejercicio 3: Buscador de películas
         // Objetivo: Pedir al usuario el nombre de una película y buscar si existe en nuestro catálogo (array).
         //
@@ -90,9 +91,100 @@ public class basicosRicoArraysVectoriales {
         }
 
         if (peliEncontrada) {
+
             System.out.println("\nSi! " + peliElegida + " está en nuestro catálogo");
         } else {
             System.out.println("\nLo sentimos, " + peliElegida + " no está en nuestro catálogo");
         }
+
+
+        // Ejercicio 4: Fusión de convocatorias
+        // Objetivo: Crear dos arrays (dos equipos) y combinarlos en un tercer array más grande.
+        //
+        // Conceptos: Declarar múltiples arrays, System.arraycopy() o bucles for para copiar.
+
+        String[] equipoBarca = new String[4];
+
+        equipoBarca[0] = "Iniesta";
+        equipoBarca[1] = "Messi";
+        equipoBarca[2] = "Lamine";
+        equipoBarca[3] = "Xavi";
+
+        String[] equipoMadrid = new String[3];
+
+        equipoMadrid[0] = "Cristiano";
+        equipoMadrid[1] = "Benzema";
+        equipoMadrid[2] = "Casillas";
+
+        // Calculamos el tamaño para el array combinado
+        int longitudBarca = equipoBarca.length;
+        int longitudMadrid = equipoMadrid.length;
+        int logitudTotal = longitudBarca + longitudMadrid;
+
+        // Creamos el array combinado (vacío por ahora)
+        String[] equipoCombinado = new String[logitudTotal];
+
+        System.out.println("\n---Fusionando Equipos---");
+
+        // Copiamos el primer equipo al principio (posición 0)
+        System.arraycopy(equipoBarca, 0, equipoCombinado, 0, longitudBarca);
+
+        // Copiamos el segundo equipo donde terminó el anterior
+        System.arraycopy(equipoMadrid, 0, equipoCombinado, longitudBarca, longitudMadrid);
+
+        // Mostramos el resultado
+        for (int i = 0; i < equipoCombinado.length; i++) {
+            System.out.print("\nEquipo Fusión: " + equipoCombinado[i]);
+        }
+*/
+
+        // Ejercicio 5: Batalla de coches
+        // Objetivo: Tienes dos arrays que representan las estadísticas (Velocidad, Aceleración, Manejo) de dos coches.
+        // Compararlos "posición a posición" para ver quién gana en cada estadística.
+        //
+        // Conceptos: Arrays paralelos, recorrer con for, if-else if-else.
+
+        int[] ferrari = new int[3];
+
+        ferrari[0] = 340;
+        ferrari[1] = 3;
+        ferrari[2] = 94;
+
+        int[] porsche = new int[3];
+
+        porsche[0] = 320;
+        porsche[1] = 3;
+        porsche[2] = 98;
+
+        int victoriasFerrari = 0;
+        int victoriasPorsche = 0;
+
+        System.out.println("\n--- COMPARATIVA FERRARI vs PORSCHE ---");
+
+        for (int i = 0; i < ferrari.length; i++) {
+
+            String categoria = " ";
+            if (i == 0) {
+                categoria = "Velocidad";
+            } else if (i == 1) {
+                categoria = "Aceleración";
+            } else if (i == 2){
+                categoria = "Manejo";
+            }
+
+            if (ferrari[i] > porsche[i]) {
+                System.out.println(categoria + ": Gana Ferrari" + " (" + ferrari[i] + ")");
+                victoriasFerrari++;
+            } else if (porsche[i] > ferrari[i]) {
+                System.out.println(categoria + ": Gana Porsche" + " (" + porsche[i] + ")");
+                victoriasPorsche++;
+            } else {
+                System.out.println(categoria + ": Empate");
+            }
+
+        }
+        System.out.println("\n--- RESULTADO FINAL ---");
+        System.out.println("Victorias Ferrari: " + victoriasFerrari);
+        System.out.println("Victorias Porsche: " + victoriasPorsche);
     }
 }
